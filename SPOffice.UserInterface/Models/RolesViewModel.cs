06c1cd7557cbfadce94add3d56a928eee36741cb
suvaneeth <suvaneeth@gmail.com>
2017-08-16 10:56:31 +0300

    base3

---
+++ SPOffice.UserInterface/Models/RolesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserInterface.Models
+{
+    public class RolesViewModel
+    {
+        public Guid? ID { get; set; }
+
+        [Required(ErrorMessage = "Please Select Application Name")]
+        public Guid? AppID { get; set; }
+
+        public string ApplicationName { get; set; }
+        
+
+        [Required(ErrorMessage = "Please enter Role Name")]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Role Description")]
+        [Display(Name = "Role Description")]
+        public string RoleDescription { get; set; }
+
+        public List<SelectListItem> ApplicationList { get; set; }
+
+        public CommonViewModel commonDetails { get; set; }
+        public string PathToAttachment { get; set; }
+        public HttpPostedFile FileUpload { get; set; }
+        public string Name1 { get; set; }
+
+    }
+}
