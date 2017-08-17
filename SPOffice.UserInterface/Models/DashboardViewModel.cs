90afcaf7081431379aafbdcdb99bece6fe69965b
suvaneeth <suvaneeth@gmail.com>
2017-08-17 15:55:38 +0300

    base9

---
+++ SPOffice.UserInterface/Models/DashboardViewModel.cs
@@ -0,0 +1,34 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserInterface.Models
+{
+    public class DashboardViewModel
+    {
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string TypeDesc { get; set; }
+        public string OpeningPaymentMode { get; set; }
+        public decimal OpeningBalance { get; set; }
+        public DateTime OpeningAsOfDate { get; set; }
+        public bool ISEmploy { get; set; }
+        public decimal Amount { get; set; }
+        public string account { get; set; }
+    }
+
+    public class AdminDashboardViewModel
+    {
+    }
+
+    
+    
+     
+
+    
+}
+
+
+ 
