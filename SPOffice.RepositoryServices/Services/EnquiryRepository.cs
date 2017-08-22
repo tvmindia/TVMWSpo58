469dcb1dbc13eba7a8f614583fd4116c63a6a19b
Praveena M S <Praveena M S@DESKTOP-P2SAJPJ>
2017-08-22 14:45:53 +0530

    enquiry

--- SPOffice.RepositoryServices/Services/EnquiryRepository.cs
+++ SPOffice.RepositoryServices/Services/EnquiryRepository.cs
@@ -174,7 +174,9 @@ namespace SPOffice.RepositoryServices.Services
                                             _enquiryObj.CompanyName = (sdr["CompanyName"].ToString() != "" ? sdr["CompanyName"].ToString() : _enquiryObj.CompanyName);
                                             _enquiryObj.EnquiryNo = (sdr["EnquiryNo"].ToString() != "" ? sdr["EnquiryNo"].ToString() : _enquiryObj.EnquiryNo);
                                             _enquiryObj.Mobile = (sdr["Mobile"].ToString() != "" ? sdr["Mobile"].ToString() : _enquiryObj.Mobile);
-                                        _enquiryObj.EnquiryDate = (sdr["EnquiryDate"].ToString() != "" ? DateTime.Parse(sdr["EnquiryDate"].ToString()).ToString(s.dateformat) : _enquiryObj.EnquiryDate);
+                                            _enquiryObj.Email = (sdr["Email"].ToString() != "" ? sdr["Email"].ToString() : _enquiryObj.Email);
+                                            _enquiryObj.GeneralNotes = (sdr["GeneralNotes"].ToString() != "" ? sdr["GeneralNotes"].ToString() : _enquiryObj.GeneralNotes);
+                                            _enquiryObj.EnquiryDate = (sdr["EnquiryDate"].ToString() != "" ? DateTime.Parse(sdr["EnquiryDate"].ToString()).ToString(s.dateformat) : _enquiryObj.EnquiryDate);
                                     }
                                     EnquiryList.Add(_enquiryObj);
                                 }
