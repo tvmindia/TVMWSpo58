62aae35190557c607c85d46f4bccdb62ebf2d5a5
suvaneeth <suvaneeth@gmail.com>
2017-11-27 13:21:06 +0300

    redirection issue from dashboard avoided

--- SPOffice.UserInterface/Controllers/QuotationController.cs
+++ SPOffice.UserInterface/Controllers/QuotationController.cs
@@ -36,8 +36,14 @@ namespace SPOffice.UserInterface.Controllers
         [AuthSecurityFilter(ProjectObject = "Quotation", Mode = "R")]
         public ActionResult Index(string id)
         {
-            ViewBag.value = id;
-            ViewBag.filter = id;
+            if (id == "Draft" || id == "OnHold" || id == "InProgress" || id == "Closed") {
+                ViewBag.filter = id;
+            }
+            else if (id != "") {
+                ViewBag.value = id;
+            }
+          
+           
             QuoteHeaderViewModel quoteHeaderVM = new QuoteHeaderViewModel();
             List<SelectListItem> selectListItem = new List<SelectListItem>();
             List<CustomerViewModel> CustList = Mapper.Map<List<Customer>, List<CustomerViewModel>>(_customerBusiness.GetAllCustomers());
