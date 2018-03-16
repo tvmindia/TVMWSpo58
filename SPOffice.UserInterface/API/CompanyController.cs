1ff280681fc6eeca2c7b49128c2e5397d6c66f69
Praveena M S <Praveena M S@TCMPF>
2018-03-16 13:57:32 +0530

    Company and RawMaterial API

---
+++ SPOffice.UserInterface/API/CompanyController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using SPOffice.BusinessService.Contracts;
+using SPOffice.DataAccessObject.DTO;
+using SPOffice.UserInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SPOffice.UserInterface.API
+{
+    public class CompanyController : ApiController
+    {
+        AppConst c = new AppConst();
+        ICompanyBusiness _companyBusiness;
+        public CompanyController(ICompanyBusiness companyBusiness)
+        {
+            _companyBusiness = companyBusiness;
+        }
+        #region GetAllCompanies
+        [HttpGet]
+        public string GetAllCompanies()
+        {
+            try
+            {
+                List<CompanyViewModel> CompaniesList = Mapper.Map<List<Company>, List<CompanyViewModel>>(_companyBusiness.GetAllCompanies());
+                return JsonConvert.SerializeObject(new { Result = true, Records = CompaniesList });
+            }
+            catch (Exception ex)
+            {
+                AppConstMessage cm = c.GetMessage(ex.Message);
+                return JsonConvert.SerializeObject(new { Result = false, Message = cm.Message });
+            }
+        }
+        #endregion  GetAllCompanies
+
+    }
+}
---
+++ SPOffice.UserInterface/API/RawMaterialController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using SPOffice.BusinessService.Contracts;
+using SPOffice.DataAccessObject.DTO;
+using SPOffice.UserInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SPOffice.UserInterface.API
+{
+    public class RawMaterialController : ApiController
+    {
+        AppConst c = new AppConst();
+        IRawMaterialBusiness _rawMaterialBusiness;
+        IUnitsBusiness _unitsBusiness;
+        public RawMaterialController(IRawMaterialBusiness rawMaterialBusiness, IUnitsBusiness unitsBusiness)
+        {
+            _rawMaterialBusiness = rawMaterialBusiness;
+            _unitsBusiness = unitsBusiness;
+        }
+        #region GetAllMaterialType
+        [HttpGet]
+        public string GetAllRawMaterials()
+        {
+            try
+            {
+                List<RawMaterialViewModel> rawMaterialList = Mapper.Map<List<RawMaterial>, List<RawMaterialViewModel>>(_rawMaterialBusiness.GetAllRawMaterial());
+                return JsonConvert.SerializeObject(new { Result = true, Records = rawMaterialList });
+            }
+            catch (Exception ex)
+            {
+                AppConstMessage cm = c.GetMessage(ex.Message);
+                return JsonConvert.SerializeObject(new { Result = false, Message = cm.Message });
+            }
+        }
+        #endregion  GetAllMaterialType
+    }
+}
